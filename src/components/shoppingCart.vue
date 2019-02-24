a1d0d17c2b8b34c7f7fcdf7caea7b8149e204de6
RitaKrasnovid <mkrasnovid@lineate.com>
2019-02-24 19:24:03 +0400

    first

---
+++ src/components/shoppingCart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Shopping Cart</h1>
+    <ul>
+      <li v-for="product in products">
+        {{ product.title }} - {{ product.price | currency }} - {{ product.quantity }}</li>
+    </ul>
+    <p>{{ total | currency}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    products () {
+      return this.$store.getters.cartProduct
+    },
+
+    total () {
+      return this.$store.getters.cartTotal
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
